229b72f6be12b0c2061b5c5652b579482deafc4c
nodoudt <efefeee1@gmail.com>
2020-01-13 22:22:44 +0900

    application.yml 설정 변경 / application-dev.yml 추가

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
-        log.info(String.format("IndexController.index: $s", user));
+        log.debug(String.format("IndexController.index: %s", user));
 
         model.addAttribute("posts", postsService.findAllDesc());
 
